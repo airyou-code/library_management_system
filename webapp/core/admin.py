78750b1cfa368cc219679273d46d3f20187c31c6
Artem Kirillov <artem.k3001@gmail.com>
2024-12-13 14:53:27 +0100

    Add abstract CoreModel and generic class for ModelAdmin

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -1,4 +1,99 @@
+from django.utils.translation import gettext_lazy as _
+from core.models import CoreModel
 from django.contrib import admin
 
 # Register your models here.
 admin.site.site_header = "Library Management System"
+
+
+class CoreAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing the CoreModel.
+
+    This class provides customization for the Django admin interface to
+    manage the CoreModel. It defines actions, ordering, and sets
+    fields as readonly or changeable based on certain conditions.
+    """
+    custom_fields: list = []
+    change_fields: list = []
+    # ordering = ("state")
+    base_fields = CoreModel.get_all_fields()
+    base_readonly = base_fields
+
+    def get_fieldsets(self, request, obj=None):
+        """Dynamically add the 'Meta Data' fieldset for superusers."""
+        fieldsets = super().get_fieldsets(request, obj)
+
+        # Only show "Meta Data" to superusers or admin
+        if request.user.is_superuser or request.user.is_admin:
+            fieldsets += (
+                ("Meta Data", {
+                    'fields': self.base_fields,
+                }),
+            )
+        return fieldsets
+
+    def save_model(self, request, obj, form, change):
+        """
+        Save the CoreModel instance after modification.
+
+        This method is called when a CoreModel instance is saved
+        in the admin interface. It automatically sets the "user_created"
+        and "user_updated" fields based on the request user.
+
+        Args:
+            request (HttpRequest):
+                The HTTP request object.
+            obj (CoreModel):
+                The CoreModel instance being saved.
+            form (ModelForm):
+                The form used to modify the instance.
+            change (bool):
+                A boolean indicating if this is an existing
+                instance being changed or a new one being created.
+
+        Returns:
+            None
+        """
+        if not obj.user_created:
+            obj.user_created = request.user
+            obj.user_updated = request.user
+        else:
+            obj.user_updated = request.user
+        super(CoreAdmin, self).save_model(request, obj, form, change)
+
+    def get_readonly_fields(self, request, obj=None) -> list:
+        """
+        Retrieve the readonly fields based on the state of
+        the CoreModel instance.
+
+        This method is called to determine the readonly fields for
+        the admin interface based on the state of the CoreModel instance.
+
+        Args:
+            request (HttpRequest):
+                The HTTP request object.
+            obj (CoreModel, optional):
+                The CoreModel instance.
+                Defaults to None.
+
+        Returns:
+            list: A list of readonly fields.
+        """
+
+        if obj and self.change_fields and obj.time_created:
+
+            return [
+                field.name for field in (
+                    *self.model._meta.get_fields(),
+                ) if field.name not in self.change_fields
+            ] + [
+                *self.base_readonly,
+                *self.custom_fields
+            ]
+
+        return [
+            *self.readonly_fields,
+            *self.base_readonly,
+            *self.custom_fields
+        ]
--- webapp/core/models.py
+++ webapp/core/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CoreModel(models.Model):
+
+    soft_delete = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Created At")
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_("Updated At")
+    )
+    deleted_at = models.DateTimeField(
+        _("Deleted At"), editable=False,
+        null=True, blank=True
+    )
+
+    user_created = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_("created by user"),
+        editable=False, null=True, blank=True,
+        related_name="+", on_delete=models.SET_NULL
+    )
+    user_updated = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_("updated by user"),
+        editable=False, null=True, blank=True,
+        related_name="+", on_delete=models.SET_NULL
+    )
+    user_deleted = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_("deleted by user"),
+        editable=False, null=True, blank=True,
+        related_name="+", on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_all_fields(cls):
+        """Return a list of all fields in the model."""
+        return [field.name for field in cls._meta.get_fields()]
+
+    @classmethod
+    def get_simple_fields(cls):
+        """Return a list of all non-relation fields."""
+        return [field.name for field in cls._meta.get_fields() if not field.is_relation]
+
+    @classmethod
+    def get_related_fields(cls):
+        """Return a list of all relation fields."""
+        return [field.name for field in cls._meta.get_fields() if field.is_relation]
+
+    def delete(self, soft=True, *args, **kwargs):
+        """
+        Soft-delete or hard-delete the object based on 'Soft' parameter.
+
+        This method allows soft-deleting the object by marking it as removed
+        and setting the 'time_deleted' attribute. If 'soft' parameter is False,
+        the object is hard-deleted from the database.
+
+        Args:
+            soft (bool): A boolean indicating whether to soft-delete the
+                object (default is True).
+
+        Returns:
+            None
+        """
+
+        if settings.DEBUG:
+            soft = False
+
+        if soft:
+            # soft-delete instance
+            self.soft_delete = True
+            self.time_deleted = timezone.now()
+            self.save()
+        else:
+            # delete from DB
+            super().delete(*args, **kwargs)
