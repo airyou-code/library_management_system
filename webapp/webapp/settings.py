6c3d6057bb0d2cbb4c5fef9ead302947d167cc84
Artem Kirillov <artem.k3001@gmail.com>
2024-12-16 19:09:18 +0100

    Add django_countries

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -60,6 +60,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 INSTALLED_APPS = [
     "jazzmin",
+    'django_countries',
 
     "django.contrib.admin",
     "django.contrib.auth",
